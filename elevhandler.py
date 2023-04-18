1119818be949751300a8e3ec7d0136d864317f5c
Martin Esbjörnsson <maes0625@storsjogymnasiet.se>
2023-04-18 10:08:43 +0200

    Skapat alla filer och börjat

---
+++ elevhandler.py
@@ -0,0 +1,19 @@
+import elev
+
+class ElevHandler:
+
+    #Konstruktor-----------------------------------
+    def __init__(self):
+        self.elevlist = []
+
+    def print_meny(self):
+        print("------------------------")
+        print("\n\tMENY\n")
+        print("\t1. Lista elever")
+        print("\t2. Lägg till elev")
+        print("\t3. Ta bort elev")
+        print("\t4. Spara och Avsluta")
+
+        val = input("Gör ett val: ")
+
+        return val
